be989a50cc450ca2821de83533aeac2ba14950f1
Gabriel Barbosa <gahbarbosa7@gmail.com>
2022-10-21 10:54:00 -0300

    page 3 dropdown

--- src/pages/FormStep3/index.tsx
+++ src/pages/FormStep3/index.tsx
@@ -1,12 +1,13 @@
+import { ChangeEvent, useEffect, useState } from 'react';
 import { useHistory, Link } from 'react-router-dom';
 import * as C from './styles';
 import { useForm, FormActions } from '../../contexts/FormContext';
 import { Theme } from '../../components/Theme';
-import { ChangeEvent, useEffect } from 'react';
 
 export const FormStep3 = () => {
     const history = useHistory();
     const { state, dispatch } = useForm();
+    const [open, setOpen] = useState(false)
 
     useEffect(() => {
         if(state.name === '') {
@@ -20,15 +21,31 @@ export const FormStep3 = () => {
     }, []);
 
     const handleNextStep = () => {
-        if(state.operatorName !== '' && state.operatorValue !== '') {
+        var pass = true
+        
+        if(state.operatorName === '' || typeof state.operatorName === 'undefined'){
+            const operatorName = document.getElementById('operatorName') as HTMLInputElement | null;
+            operatorName?.classList.add('required')
+            pass = false
+        }
+
+        if(state.operatorValue === '' || typeof state.operatorValue === 'undefined'){
+            const operatorValue = document.getElementById('operatorValue') as HTMLInputElement | null;
+            operatorValue?.classList.add('required')
+            pass = false
+        }
+
+        if(pass) {
             history.push('/step4');
-        } 
+        } else {
+            alert('preencha os dados')
+        }
     }
 
-    const handleOperatorName = (e: ChangeEvent<HTMLInputElement>) => {
+    const handleOperatorName = (value: string) => {
         dispatch({
             type: FormActions.setOperatorName,
-            payload: e.target.value
+            payload: value
         });
     }
 
@@ -48,13 +65,47 @@ export const FormStep3 = () => {
 
                 <hr/>
 
+                <div className='dropdown'>
+                    <button onClick={()=>{setOpen(!open)}}>
+                        Opções
+                    </button>
+                    {open ? (
+                        <ul className="menu">
+                            <li className="menu-item">
+                                <button onClick={()=>{handleOperatorName('Omint'); setOpen(false)}}>Omint</button>
+                            </li>
+                            <li className="menu-item">
+                                <button onClick={()=>{handleOperatorName('Sulamérica'); setOpen(false)}}>Sulamérica</button>
+                            </li>
+                            <li className="menu-item">
+                                <button onClick={()=>{handleOperatorName('Bradesco Seguros'); setOpen(false)}}>Bradesco Seguros</button>
+                            </li>
+                            <li className="menu-item">
+                                <button onClick={()=>{handleOperatorName('Unimed'); setOpen(false)}}>Unimed</button>
+                            </li>
+                            <li className="menu-item">
+                                <button onClick={()=>{handleOperatorName('Notredame Intermédica'); setOpen(false)}}>Notredame Intermédica</button>
+                            </li>
+                            <li className="menu-item">
+                                <button onClick={()=>{handleOperatorName('Hapvida'); setOpen(false)}}>Hapvida</button>
+                            </li>
+                            <li className="menu-item">
+                                <button onClick={()=>{handleOperatorName(''); setOpen(false)}}>Outro</button>
+                            </li>
+                        
+                        </ul>
+                    ) : null}
+
+                </div>
+
                 <label>
                     Qual o nome da sua operadora?
                     <input
+                        id='operatorName'
                         type="text"
                         autoFocus
                         value={state.operatorName}
-                        onChange={handleOperatorName}
+                        onChange={(e)=>{handleOperatorName(e.target.value)}}
                     />
                 </label>
 
@@ -62,6 +113,7 @@ export const FormStep3 = () => {
                 <label>
                     Qual o valor da sua fatura hoje?
                     <input
+                        id='operatorValue'
                         type="text"
                         autoFocus
                         value={state.operatorValue}
--- src/pages/FormStep3/styles.ts
+++ src/pages/FormStep3/styles.ts
@@ -53,4 +53,49 @@ export const Container = styled.div`
         padding: 20px 40px;
         color: #B8B8D4;
     }
+
+    .dropdown {
+        position: relative;
+        margin: 20px 10px;
+        width: 100%;
+    }
+
+    .dropdown button {
+        width: 100%;
+        height:100%;
+        border-radius: 10px;
+        text-align: center
+    }
+      
+    .menu {
+        position: absolute;
+        list-style-type: none;
+        margin: 5px 0;
+        padding: 0;
+        border: 1px solid grey;
+        width: 200px;
+    }
+    
+    .menu > li {
+        margin: 0;
+        background-color: Black;
+    }
+    
+    .menu > li:hover {
+        background-color: lightgray;
+    }
+    
+    .menu > li > button {
+        width: 100%;
+        height: 100%;
+        text-align: left;
+        
+        background: none;
+        color: inherit;
+        border: none;
+        padding: 5px;
+        margin: 0;
+        font: inherit;
+        cursor: pointer;
+    }
 `;
